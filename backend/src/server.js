326200b2b32917a0eeb131db1b38a44dc177b1e2
iamashruu <ashrafuddin882@gmail.com>
2026-02-04 16:54:14 +0600

    add deployment code

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,11 @@
 import express from "express";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+
 const app = express();
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
@@ -10,6 +14,15 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+
+//make ready for deployment
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+  app.get("*",(_,res)=>{
+    res.sendFile(path.resolve(__dirname,"../frontend","dist","index.html")); // serve index.html for any other route -> we can write together: ../frontend/dist/index.html
+  });
+};
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
